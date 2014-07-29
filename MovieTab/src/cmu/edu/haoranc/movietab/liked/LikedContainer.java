d59b55a42f3d5fe1b36e2f4093fccfbe89c40ed7
migrantcheng <migrantcheng@gmail.com>
2014-07-29 03:31:29 -0400

    update for push

--- MovieTab/src/cmu/edu/haoranc/movietab/liked/LikedContainer.java
+++ MovieTab/src/cmu/edu/haoranc/movietab/liked/LikedContainer.java
@@ -4,12 +4,10 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import cmu.edu.haoranc.movietab.R;
-import cmu.edu.haoranc.movietab.TabsPagerAdapter;
 
 public class LikedContainer extends Fragment {
 	
